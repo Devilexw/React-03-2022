23634864013477ae49a0c7175ba346e526f32e38
Devilexw <102145421+Devilexw@users.noreply.github.com>
2022-03-22 21:13:39 +0200

    algus

---
+++ veebipood/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// kommentaar
+// ctrl + ä aktiveerib koodi
+
+//README -  tutvustus reacti osas
+// packae.json - koodilõikude kogumik
+
+//react vers 15 -> 17
+//cmd 'npm install'
+
+//gitignore - githubi ei pane
+//node_module - kood mida react vajab
+
+//public - pidid, failid
+
+//src - kood - .js ja .css
+
